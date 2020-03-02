a8ec5bf4793730c51d6c45f3e33e10b2e1cf97c1
Konstantin <prokhorenko_kp@shopandshow.ru>
2020-03-02 11:39:24 +0300

    Updates

---
+++ precache-manifest.bcb878e71022b77c2d7b11813c41fcf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080ad5215526eb3a113021a0f614b9d6",
+    "url": "/dnd_table/index.html"
+  },
+  {
+    "revision": "4f47dad1d13cce74a587",
+    "url": "/dnd_table/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6c528e784155887c0e07",
+    "url": "/dnd_table/static/css/main.6535bbbf.chunk.css"
+  },
+  {
+    "revision": "4f47dad1d13cce74a587",
+    "url": "/dnd_table/static/js/2.c393934c.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/dnd_table/static/js/2.c393934c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c528e784155887c0e07",
+    "url": "/dnd_table/static/js/main.05d3f4dd.chunk.js"
+  },
+  {
+    "revision": "3e4400dcb3c6af079925",
+    "url": "/dnd_table/static/js/runtime-main.0e35bb69.js"
+  }
+]);
